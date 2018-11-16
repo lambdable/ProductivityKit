ce312b41bad3b916767dafa6af141e455c576ae0
Zaim Ramlan <zaem.muz@gmail.com>
2018-11-16 09:45:09 +0800

    added uicolor utility

---
+++ UIColor/UIColor+Utility.swift
@@ -0,0 +1,28 @@
+
+import UIKit
+
+extension UIColor {
+    
+    /// Utility class to get Hex values
+    class Hex {
+        
+        /// Returns a UIColor instance for the given RGBA color values.
+        ///
+        /// - Parameters:
+        ///   - red: A CGFloat to define the 'red' colour intesity.
+        ///   - green: A CGFloat to define the 'green' colour intesity.
+        ///   - blue: A CGFloat to define the 'blue' colour intesity.
+        ///   - alpha: A CGFloat to define the color's opacity.
+        /// - Returns: A UIColor instance representing the RGBA color values.
+        class func rgba(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat, _ alpha: CGFloat) -> UIColor {
+            let redValue   = red/255
+            let greenValue = green/255
+            let blueValue  = blue/255
+            
+            return UIColor(red: redValue, green: greenValue, blue: blueValue, alpha: alpha)
+        }
+        
+        // example color definitions
+        static let _666666 = rgba(96, 96, 96, 1)
+    }
+}
